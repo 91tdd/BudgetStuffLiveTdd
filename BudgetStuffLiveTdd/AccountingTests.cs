2145c58295a6aec7f31a8ba6443f170a1de4d94a
hatelove <jay.hatelove@gmail.com>
2018-03-29 16:05:22 +0800

    pass test: no budgets

---
+++ BudgetStuffLiveTdd/AccountingTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BudgetStuffLiveTdd
+{
+    [TestClass]
+    public class AccountingTests
+    {
+        [TestMethod]
+        public void no_budgets()
+        {
+            var accounting = new Accounting();
+            var totalAmount = accounting.TotalAmount(new DateTime(), new DateTime());
+            Assert.AreEqual(0, totalAmount);
+        }
+    }
+}
